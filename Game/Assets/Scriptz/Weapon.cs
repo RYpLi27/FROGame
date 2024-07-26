bd51830cae177045b41f090699cd7c42ae0c3e1a
RYpLi <zjanasek27@gmail.com>
2024-07-26 18:27:48 +0200

    yes

--- Game/Assets/Scriptz/Weapon.cs
+++ Game/Assets/Scriptz/Weapon.cs
@@ -4,7 +4,9 @@ public class Weapon : MonoBehaviour
 {
 
     public Transform transformPosition;
+    public float fireRate = 1.0f;
 
+    private float _nextFireTime = 0.0f;
     private void Awake()
     {
         if (transformPosition == null)
@@ -13,9 +15,13 @@ public class Weapon : MonoBehaviour
         }
     }
 
-    // Update is called once per frame
-    void Update()
+     void Update()
     {
-        
+        if (Time.timeSinceLevelLoad >= _nextFireTime && Input.GetAxis("Fire1") > 0)
+        {
+            Debug.Log("Fire");
+
+            _nextFireTime = Time.timeSinceLevelLoad + 1.0f / fireRate;
+        }
     }
 }
