6f27b048b43aaf2a4c88130af4e2a37bf3d290c6
RYpLi <zjanasek27@gmail.com>
2024-07-29 14:24:04 +0200

    dsa

--- Game/Assets/Scriptz/Weapon.cs
+++ Game/Assets/Scriptz/Weapon.cs
@@ -7,6 +7,10 @@ public class Weapon : MonoBehaviour
     public float fireRate = 1.0f;
 
     private float _nextFireTime = 0.0f;
+
+    private Vector3 _shootPositionl;
+    private Vector3 _hitPosition;
+
     private void Awake()
     {
         if (transformPosition == null)
@@ -33,9 +37,22 @@ public class Weapon : MonoBehaviour
         if (Physics.Raycast(transformPosition.position, transformPosition.forward, out RaycastHit hitInfo))
         {
              Debug.Log(hitInfo.collider);
+            _shootPositionl = transformPosition.position;
+            //_hitPosition = hitInfo.collider.transform.position;
+            _hitPosition = hitInfo.point;
         }
-    
+
         _nextFireTime = Time.timeSinceLevelLoad + 1.0f / fireRate;
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        if (transformPosition == null) return;
+        Gizmos.color = Color.red;
+        Gizmos.DrawRay(transformPosition.position, transformPosition.forward);
+
+        Gizmos.color = Color.magenta;
+        Gizmos.DrawLine(_hitPosition, _shootPositionl);
     }
 
 }
